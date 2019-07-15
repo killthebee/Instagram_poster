a6d7a8eda0dab8db06798507b34b6aad36cfd4b9
DAyaan <superhotfire447@yandex.ru>
2019-07-15 14:08:56 +0300

    rework

--- fetch_hubble.py
+++ fetch_hubble.py
@@ -2,41 +2,44 @@ import os
 import pathlib
 import requests
 import argparse
-pathlib.Path('images').mkdir(exist_ok=True)
+
 
 
 def get_hublpics(pic_id):
 
-    habl_url = 'http://hubblesite.org/api/v3/image/{}'.format(str(pic_id))
+    habl_url = 'http://hubblesite.org/api/v3/image/%s'%(pic_id)
     response = requests.get(habl_url)
     response.raise_for_status()
     response_unpacked = response.json()
     last_item_of_list = -1
     last_pic_kinda_url = response_unpacked['image_files'][last_item_of_list]['file_url']
-    schema = 'https:'
-    last_pic_url = schema + last_pic_kinda_url
+    last_pic_url = 'https:%s'%(last_pic_kinda_url)
     splited_l_p_url = last_pic_url.replace('.', ' ').split()
     extension = splited_l_p_url[last_item_of_list]
-    filename = str(pic_id) + '.' + extension
-    habls_pic_response = requests.get(last_pick_url, verify=False)
+    filename = '%s.%s'%(pic_id, extension)
+    habls_pic_response = requests.get(last_pic_url, verify=False)
     with open(os.path.join('images',filename), 'wb') as file:
         file.write(habls_pic_response.content)
 
 
 def main():
+
+    pathlib.Path('images').mkdir(exist_ok=True)
     parser = argparse.ArgumentParser(
       description='Choose your favorite Hubble collection: holiday_cards, wallpaper, spacecraft, news, printshop, stsci_gallery'
     )
-    parser.add_argument('collection', help='Type collection name here', default = 'spacecraft')
+    parser.add_argument('-c','--collection', help='Type collection name here', default = 'spacecraft')
     args = parser.parse_args()
     collection_name = args.collection
-    coll_url = 'http://hubblesite.org/api/v3/images?page=all&collection_name={}'.format(collection_name)
-    response = requests.get(coll_url)
+    coll_url = 'http://hubblesite.org/api/v3/images'
+    params = {'page':'all', 'collection_name':collection_name}
+    response = requests.get(coll_url, params=params )
     response.raise_for_status()
     response_unpacked = response.json()
-    for i in response_unpacked:
-        id = i['id']
-        get_hublpics(id)
+    print(response_unpacked)
+    for pic_info in response_unpacked:
+        pic_id = pic_info['id']
+        get_hublpics(pic_id)
 
 
 if __name__ == '__main__':
--- fetch_spacex.py
+++ fetch_spacex.py
@@ -1,19 +1,19 @@
 import os
 import pathlib
 import requests
-pathlib.Path('images').mkdir(exist_ok=True)
+
 
 
 def main():
 
+    pathlib.Path('images').mkdir(exist_ok=True)
     spx_url = 'https://api.spacexdata.com/v3/launches/latest'
     response = requests.get(spx_url)
     response.raise_for_status()
     response_unpacked = response.json()
     links = response_unpacked['links']['flickr_images']
-    #print(links)
     for index, link in enumerate(links, 1):
-        filename = 'spaceX' + str(index) + '.jpg'
+        filename = 'spaceX%s.jpg'%(index)
         flickr_response = requests.get(link)
         with open(os.path.join('images',filename), 'wb') as file:
             file.write(flickr_response.content)
